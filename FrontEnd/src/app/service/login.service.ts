1b265270736acf72fe70c679c9c70b7cf3d9e944
Ana <anarodriguez.p@hotmail.es>
2023-10-17 16:32:07 +0200

    added logical login service

--- FrontEnd/src/app/service/login.service.ts
+++ FrontEnd/src/app/service/login.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Login } from
+import { Login } from '../models/login.model';
  
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  private loginUrl = '/api/login';
 
   constructor(private http: HttpClient) { }
+
+  login(credentials: Login): Observable<any>{
+    return this.http.post(this.loginUrl, credentials);
+  }
 }
--- FrontEnd/src/app/views/login/login.component.ts
+++ FrontEnd/src/app/views/login/login.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-// import { LoginService } from 'src/app/service/login.service';
-import Swal  from 'sweetalert2';
+import  Swal  from 'sweetalert2';
+import { LoginService } from '../../../app/service/login.service';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-login',
@@ -12,70 +13,77 @@ export class LoginComponent {
   username: string = '';
   password: string = '';
 
-constructor( 
-  private service: LoginService,
-  private router: Router,
-  private userService: UserService){}
+  constructor(
+    private service: LoginService,
+    private router: Router,
+    private userService: UserService
+  ) {}
 
-  login(){
-    if (this. username === "felizcumpleaños" && this.password "123456"){
+  login() {
+    if (this.username === "felizcumpleaños" && this.password === "123456") {
       this.router.navigateByUrl('dashboard');
-      return;  
+      return;
     }
 
-    let bodyData: Login = {
+    const bodyData: Login = {
       username: this.username,
       password: this.password
-    }
+    };
 
-    
-    localStorage.setItem('email', this.email);
-  
     this.service.postLogin(bodyData).subscribe((resultData: any) => {
-      
-      if (resultData.message == "Email not exist") {
-        Swal.fire({
-          icon: 'error',
-          title: 'Error',
-          text: 'El email no existe',
-          customClass: 'custom-swal',
-          showConfirmButton: true,
-          width: 300, 
-          confirmButtonColor: 'rgba(29, 41, 81, 1)',
-          showCancelButton: true, 
-          cancelButtonText: 'Cancelar', 
-          showCloseButton: true, 
-          confirmButtonText: 'Registrate', 
-          
-        }).then((result) => {
-          if (result.isConfirmed) {
-            this.router.navigateByUrl('register');
-          }
-        });
-      } else if (resultData.message == "Login Success") {
-        Swal.fire({
-          title: 'Bienvenid@',
-          text: 'Logueado con éxito',
-          customClass: 'custom-swal', 
-          showConfirmButton: true,
-          width: 300, 
-          confirmButtonColor: '#008000',
-        }).then(() => {
-          localStorage.setItem('loggedInEmail', resultData.email);
-          this.userService.setLogdeInEmail(resultData.email);
-          this.router.navigateByUrl('comicList');
-        });
+      if (resultData.message === "Email not exist") {
+        this.handleEmailNotExist();
+      } else if (resultData.message === "Login Success") {
+        this.handleLoginSuccess(resultData.email);
       } else {
-        Swal.fire({
-          title: 'Error',
-          text: 'La contraseña no coincide',
-          customClass: 'custom-swal' ,
-          showConfirmButton: true,
-          width: 300, 
-          confirmButtonColor: '#ff0000',
-        });
+        this.handleLoginError();
       }
     });
   }
+
+  private handleEmailNotExist() {
+    Swal.fire({
+      icon: 'error',
+      title: 'Error',
+      text: 'El email no existe',
+      customClass: 'custom-swal',
+      showConfirmButton: true,
+      width: 300,
+      confirmButtonColor: 'rgba(29, 41, 81, 1)',
+      showCancelButton: true,
+      cancelButtonText: 'Cancelar',
+      showCloseButton: true,
+      confirmButtonText: 'Registrate',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.router.navigateByUrl('register');
+      }
+    });
+  }
+
+  private handleLoginSuccess(email: string) {
+    Swal.fire({
+      title: 'Bienvenid@',
+      text: 'Logueado con éxito',
+      customClass: 'custom-swal',
+      showConfirmButton: true,
+      width: 300,
+      confirmButtonColor: '#008000',
+    }).then(() => {
+      localStorage.setItem('loggedInEmail', email);
+      this.userService.setLogdeInEmail(email);
+      this.router.navigateByUrl('comicList');
+    });
+  }
+
+  private handleLoginError() {
+    Swal.fire({
+      title: 'Error',
+      text: 'La contraseña no coincide',
+      customClass: 'custom-swal',
+      showConfirmButton: true,
+      width: 300,
+      confirmButtonColor: '#ff0000',
+    });
   }
 }
