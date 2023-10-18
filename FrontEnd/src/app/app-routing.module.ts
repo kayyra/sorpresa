ef373996a0319c012c9c6ea00488b39379385be2
Ana <anarodriguez.p@hotmail.es>
2023-10-18 08:59:18 +0200

    added video in the header

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './views/login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 const routes: Routes = [
   {path: '', component:LoginComponent}, 
+  {path: '', component: HeaderComponent},
   {path: 'dashboard', component: DashboardComponent}
 ];
 
