02e1a994f105ef6af2bdf1214a069d2e119264ab
Ana <anarodriguez.p@hotmail.es>
2023-10-19 12:03:27 +0200

    nav menu route

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -8,10 +8,12 @@ import { ComidaComponent } from './views/comida/comida.component';
 import { FunkosComponent } from './views/funkos/funkos.component';
 import { animate } from '@angular/animations';
 import { AnimeComponent } from './views/anime/anime.component';
+import { NavMenuComponent } from './shared/nav-menu/nav-menu.component';
 
 const routes: Routes = [
   {path: '', component:LoginComponent}, 
   {path: '', component: HeaderComponent},
+  {path: 'nav-menu', component:NavMenuComponent},
   {path: 'dashboard', component: DashboardComponent},
   {path: 'videos', component: VideosComponent},
   {path: 'comida', component: ComidaComponent},
